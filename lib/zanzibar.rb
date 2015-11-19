TerribleDev/zanzibar

commit 813b171d2632f2b80b6bfa9b2b99bf3ebcf345a6

Print response to indicate when cli is no longer interactive

lib/zanzibar.rb
@@ -67,6 +67,7 @@ module Zanzibar
     def prompt_for_password
       puts "Please enter password for #{@@username}:"
       STDIN.noecho(&:gets).chomp
+      puts "Using password to login..."
     end
 
     ## Gets the wsdl document location if none is provided in the constructor

commit 293abdacdeadadbbd98783eafd41b2bacd4a8318

this is a fix for commit 813b171d2632f2b80b6bfa9b2b99bf3ebcf345a6 which broke the prompt_for_password method and resulted a nil password.

lib/zanzibar.rb
@@ -66,8 +66,9 @@ module Zanzibar
 
     def prompt_for_password
       puts "Please enter password for #{@@username}:"
-      STDIN.noecho(&:gets).chomp
-      puts "Using password to login..."
+      STDIN.noecho(&:gets).chomp.tap do
+        puts "Using password to login..."
+      end
     end
 
     ## Gets the wsdl document location if none is provided in the constructor